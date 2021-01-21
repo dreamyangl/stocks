c8a7ef35f27af7326b2f6c7588878589a9257b02
yanglingjie <1442683804@qq.com>
2021-01-21 22:02:37 +0800

    init

--- query/akTodaysharequery.py
+++ query/akTodaysharequery.py
@@ -13,16 +13,22 @@ import pandas as pd
 import numpy as np
 
 
-def mainNetTodayPurchase():
+def mainNetInflow(data):
     list = []
-    df1 = ak.stock_individual_fund_flow_rank(indicator="今日")
-    df1 = df1.drop(df1[(df1['主力净流入-净额'] == "-")].index)
-    df1 = df1[(df1['涨跌幅'] < 9) & (~df1['名称'].str.contains('ST'))]
-    df1['主力净流入-净额'] = df1['主力净流入-净额'].astype('float64')
-    df1 = df1.sort_values('主力净流入-净额', ascending=False)
-    df1 = df1[['代码', '名称', '主力净流入-净额', '大单净流入-净占比', '超大单净流入-净占比', '涨跌幅', '主力净流入-净占比', ]].head(30)
-    df1.to_csv('./todaydata.csv')
-    list.append(df1)
+    data = data.sort_values('主力净流入-净额', ascending=False)
+    data = data[['代码', '名称', '主力净流入-净额', '大单净流入-净占比', '超大单净流入-净占比', '涨跌幅', '主力净流入-净占比', ]].head(30)
+    data.to_csv('./todaydata.csv')
+    list.append(data)
+    return list
+
+
+def up(data):
+    list = []
+    data = data.sort_values('涨跌幅', ascending=False).head(10)
+    data = data.sort_values('超大单净流入-净占比', ascending=False)
+    data = data[['代码', '名称', '主力净流入-净额', '大单净流入-净占比', '超大单净流入-净占比', '涨跌幅', '主力净流入-净占比', ]]
+    data.to_csv('./up.csv')
+    list.append(data)
     return list
 
 
@@ -30,11 +36,17 @@ def executeTodayAk():
     # 上午11点半到1点不查询
     if isNotify() == False:
         return
+    data = ak.stock_individual_fund_flow_rank(indicator="今日")
+    data = data.drop(data[(data['主力净流入-净额'] == "-")].index)
+    data = data[(data['涨跌幅'] < 9) & (~data['名称'].str.contains('ST'))]
+    data['主力净流入-净额'] = data['主力净流入-净额'].astype('float64')
+    data['主力净流入-净占比'] = data['主力净流入-净占比'].astype('float64')
     streage = {
-        '今日主力净买入 字段:代码 名称 主力净流入-净额 主力净流入-净占比 涨跌幅 大单净流入-净占比 超大单净流入-净占比': mainNetTodayPurchase,
+        '今日主力净买入排序前30 字段:代码 名称 主力净流入-净额 主力净流入-净占比 涨跌幅 大单净流入-净占比 超大单净流入-净占比': mainNetInflow,
+        '涨跌幅，超大单排序前30 字段:代码 名称 主力净流入-净额 主力净流入-净占比 涨跌幅 大单净流入-净占比 超大单净流入-净占比': up,
     }
     for item, fuc in streage.items():
-        mec = fuc()
+        mec = fuc(data)
         for i in mec:
             testnotice.notify(
                 '"{0}"\n{1}'.format(item, i))
@@ -46,22 +58,19 @@ def isNotify():
     if time.hour >= 11 & time.minute > 30 & time.hour <= 13:
         return False
     # 下午三点到9点不查询    逻辑or 一个满足直接返回
-    if time.hour >=15 or time.hour < 9:
+    if time.hour >= 15 or time.hour < 9:
         return False
     return True
 
 
 if __name__ == '__main__':
-    # settings.init()
+    settings.init()
     # pd.set_option('display.float_format', lambda x: '%.2f' % x)
     # executeTodayAk()
-    print(isNotify())
+    # print(isNotify())
+    executeTodayAk()
     # index = pd.date_range('1/1/2000', periods=8)
     # data = pd.DataFrame(np.random.randn(8, 3), index=index,
     #                     columns=['A', 'B', 'C'])
     # print(data)
     # print(data.sum())
-
-    stock_zh_a_daily_qfq_df = ak.stock_zh_a_daily(symbol="sz000002", start_date="20210120", end_date="20210121",
-                                                  adjust="qfq")
-    print(stock_zh_a_daily_qfq_df)
