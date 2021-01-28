6c0ebd64c0c6a5c7ecb3f6db53f44664d07763a3
yanglingjie <imv0494@dingtalk.com>
2021-01-28 15:29:20 +0800

    添加测试爬虫

--- query/testmatalib.py
+++ query/testmatalib.py
@@ -1,4 +1,8 @@
-import matplotlib.pyplot as mplt
-import tushare as ts
-
-# 支持中文
+import matplotlib.pyplot as plt
+#mpl.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']    # 指定默认字体：解决plot不能显示中文问题
+plt.rcParams['axes.unicode_minus'] = False           # 解决保存图像是负号'-'显示为方块的问题
+import pandas as pd
+data = pd.read_csv('./data/plate_limit_up.csv')
+data.plot('所属行业','count')
+plt.show()
